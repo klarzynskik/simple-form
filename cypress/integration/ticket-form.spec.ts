181224fed012d39b6fcb135decbba7622a1eb285
Joanna Chrzanowska <jchrzanowska@clincase.com>
2022-06-08 22:22:02 +0200

    test commit

--- cypress/integration/ticket-form.spec.ts
+++ cypress/integration/ticket-form.spec.ts
@@ -1,1 +1,16 @@
-// Your tests go in here. Happy coding! 🤓
+describe('', () => {
+    it('a', () => {
+        cy.visit('/')
+        cy.get('#name').type('a')
+        cy.get('#email').type('b@b.pl')
+        cy.get('#subject').type('c')
+        cy.get('#message').type('d')
+        cy.get('button').click()
+        cy.intercept('POST', 'https://api.livechatinc.com/v2/tickets/new', {
+            statusCode: 500,
+            body: {
+                error: 'Internal Server Error',
+            },
+        })
+    })
+})
